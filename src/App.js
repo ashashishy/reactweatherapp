918ee32dd5fc49c5f2dcee12a679ca99363ab8cc
ashashishy <ash.ashish.y@gmail.com>
2020-05-17 07:46:25 +0530

    added constructor

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={lat:null};
+    }
+    //react says we need to define render method
     render(){
         window.navigator.geolocation.getCurrentPosition(
             position => console.log(position),
