1c4dcfba353ab9110b4a9086f60d727e74f2b05f
ashashishy <ash.ashish.y@gmail.com>
2020-05-18 07:44:58 +0530

    building the weather app

--- src/App.js
+++ src/App.js
@@ -2,15 +2,31 @@ import React from 'react';
 class App extends React.Component{
     constructor(props){
         super(props);
-        this.state={lat:null};
+        //this is the only time we do direct assignment
+        this.state={lat:null, errorMessage:''};
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                //update state object we call setstate
+                this.setState({lat: position.coords.latitude, lon:position.coords.longitude});
+            },
+            err => {
+                this.setState({errorMessage:err.message})
+            }
+        );
     }
+    
     //react says we need to define render method
     render(){
-        window.navigator.geolocation.getCurrentPosition(
-            position => console.log(position),
-            err => console.log(err)
-        )
-        return <h1>Lattitude:</h1>
+        if(this.state.errorMessage && !this.state.lat){
+            return <h1>Error:{this.state.errorMessage}</h1>
+        }else if(!this.state.errorMessage && this.state.lat)
+        {
+        return <h1>Lattitude:{this.state.lat}</h1>}
+        else{
+            return <h1>Loading!</h1>
+        }
+
     }
 };
 export default App;
