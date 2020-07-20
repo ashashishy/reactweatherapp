bcb6a7de4a0d257bbf674012df3e998499073f5d
ashashishy <ash.ashish.y@gmail.com>
2020-07-20 09:01:36 +0530

    react blog comment

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App'
-ReactDOM.render(<App />, document.getElementById('root'));
+import ReactDom from 'react-dom';
+import Comment from './Comment'
+import ApprovalCard from './ApprovalCard'
+const App=()=>{
+    return (
+    <div className="ui container comments">
+    <ApprovalCard ><Comment author="Sam" comment="New blog comment" /></ApprovalCard>
+    
+    <ApprovalCard ><Comment author="Jam" comment="newer blog comment" /></ApprovalCard>
+    <ApprovalCard ><Comment author="Lam" comment="Newest blog comment" /></ApprovalCard>
+    </div>
+    );
+}
+
+ReactDom.render(<App />, document.getElementById('root')); 
