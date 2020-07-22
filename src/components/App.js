da9b50cd485eadbad16c263045607b664687a3df
ashashishy <ash.ashish.y@gmail.com>
2020-07-22 09:15:47 +0530

    React pics

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import unsplash from '../api/unsplash';
+import SearchBar from './SearchBar'
+class App extends React.Component {
+
+    state={image:[]};
+     onSearchSubmit=async (term)=>{
+     const response=await unsplash.get('/search/photos',{
+            params:{query: term}   
+        });
+        this.setState({image:response.data.results});
+        
+    }
+    
+    render(){
+        return (
+        <div className="ui container" style={{marginTop:'10px'}}>
+            <SearchBar onSubmit={this.onSearchSubmit} />
+            Founds:{this.state.image.length} images
+        </div>
+    )
+}
+}
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,35 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import SeasonDisplay from './SeasonDisplay'
-class App extends React.Component{
-    state={lat:null, errorMessage:''};
+import App from './components/App'
 
-    componentDidMount(){
-    window.navigator.geolocation.getCurrentPosition(
-        position=> {
-            this.setState({lat:position.coords.latitude});
-        }, 
-        err=> {
-            this.setState({errorMessage:err.message});
-        }
-    );
-    }
-
-    renderContent(){
-        if(!this.state.lat && this.state.errorMessage){
-            return <div>Error:{this.state.errorMessage}</div>
-        }
-        else if(this.state.lat && !this.state.errorMessage){
-            return <SeasonDisplay lat={this.state.lat} />
-        }
-        else {
-            return <div>Loading...</div>
-        }
-    }
-    render(){
-       return <div>{this.renderContent()}</div>
-    }
-}
-
-
-ReactDom.render(<App />, document.getElementById('root')); 
+ReactDom.render(
+    <App />,
+    document.querySelector('#root')
+);
